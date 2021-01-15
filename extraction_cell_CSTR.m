ac59b87ba5f92be8b7de30b17769316c22c1cfd0
Matthew Lam <mrlam@edu.uwaterloo.ca>
2021-01-15 13:41:10 -0500

    Add files via upload
    Working for one species at cathode (Copper) and for a very short time (1 microsecond). Need to adjust current/volume parameters so that system can reach some steady state?

--- extraction_cell_CSTR.m
+++ extraction_cell_CSTR.m
@@ -4,26 +4,29 @@
 	To Add: Applied Voltage calc. Non-constant volume?
 %} 
 clear all
-global R F temp pressure volume Q;
+global R F;
 %universal constants
 R = 8.314; %J/(mol K)
 F = 96485.3329; %C/mol
 % system parameters
+global temp;
 temp = 298; %K
 pressure = 1; % atm
-volume = 10; %L
-Q = 0.01; % L/s (flowrate)
+volume = 400; %L
+Q = 1; % L/s (flowrate)
 %{
 	Reactions
 	Anodic: Fe2+ --> Fe3+ + e-
 	Cathodic: Cu2+ + 2e- --> Cu(s)
 %}
+%Defining constants as global variables for use in cell solver function
+global z_Cu z_Fe i0_Cu i0_Fe alpha_Cu alpha_Fe
 % # of electrons in rxn
 z_Cu = 2;
 z_Fe = 1;
 % exchange current densities
 i0_Cu = 5E-5; %A/cm^2
-i0_Fe = 5E-5; %A/cm^2
+i0_Fe = 5E-8; %A/cm^2
 % charge transfer coefficients
 alpha_Cu = 0.5;
 alpha_Fe = 0.5;
@@ -34,30 +37,30 @@ Eo_Fe = 0.77; %V
 gamma_Cu2 = 1;
 gamma_Fe2 = 1;
 gamma_Fe3 = 1;
+
 %initial concentrations in mol/L
 %Outlet (In CSTR)
-Ci_Cu2 = 0.5;
+Ci_Cu2 = 0.1;
 Ci_Fe2 = 0.1;
 Ci_Fe3 = 0.001; 
 %inlet concentrations
 C_Cu2_o = 0.7;
-C_Fe2_o = 0.4;
-C_Fe3_o = 0.1;
+C_Fe2_o = 0.7;
+C_Fe3_o = 0.05;
 
 %Electrode areas
 S_cat = 100; %cm^2
 S_an = 100; %cm^2
-
 %Applied Voltage (potentiostat)
-Vapp = 0.8; %V
+V_app = 4; %V
 
 %Mass balance on generic ion in reactor
 dC_dt = @(C, C_o, I, z) ((C_o-C)*Q + I*F/z)/volume;
 
 %Solver
-tfinal = 1; %total time in seconds
-h = 1; %Step size = 1 s
-
+tfinal = 0.001; %total time in seconds
+h = 0.00001; %Step size = 1 s
+t = [0:h:tfinal];
 %initializing concentrations
 C_Cu2 = zeros(1, tfinal/h+1);
 C_Fe2 = zeros(1, tfinal/h+1);
@@ -65,51 +68,65 @@ C_Fe3 = zeros(1, tfinal/h+1);
 C_Cu2(1) = Ci_Cu2;
 C_Fe2(1) = Ci_Fe2;
 C_Fe3(1) = Ci_Fe3;
-
 %Nernst potential arrays
 Erev_Cu = zeros(1, tfinal/h+1);
 Erev_Fe = zeros(1, tfinal/h+1);
 Erev_Cu(1) = Eo_Cu - R*temp/(z_Cu*F)*log(1/(gamma_Cu2*C_Cu2(1)));
 Erev_Fe(1) = Eo_Fe - R*temp/(z_Fe*F)*log(gamma_Fe2*C_Fe2(1)/gamma_Fe3*C_Fe3(1));
-r_sol = ones(1, tfinal/h)*10; %ohms, add calculation for this later
+%Resistances
+r_sol = ones(1, tfinal/h); %ohms, add calculation for this later
 r_hardware = 0; %ohms
+%Currents, potential, overpotential arrays
+I_Cu = zeros(1, tfinal/h+1);
+I_Fe = zeros(1, tfinal/h+1);
+E_an = zeros(1, tfinal/h+1);
+E_cat = zeros(1, tfinal/h+1);
 for k = 1:1:(tfinal/h)
-    %Loop to solve for current
-    j = 1;
-    max_iter = 10000;
-    I_an = zeros(1, max_iter);
-    eta_an = ones(1, max_iter);
-    eta_Cu = zeros(1, max_iter);
-    I_Cu = zeros(1, max_iter);
-    I_cat = zeros(1, max_iter);
-    error = zeros(1, max_iter);
-    pct_error = ones(1, max_iter);
-    I_an(j) = 0.1*(Vapp - abs(Erev_Cu(k)-Erev_Fe(k)) - 0.1)/(r_sol(j)+r_hardware); %Guess for current, in A, assuming no overpotential
-    while pct_error > 0.1
-        %solve for anodic overpotential using B-V equation
-        i_BV_an = @(overpot) i_BV(overpot, i0_Fe, alpha_Fe, z_Fe, temp) - I_an(j)/S_an;
-        eta_an(j) = fzero(i_BV_an, 0.1);
-        %solve for cathodic overpotential of copper reaction
-        %eta_Cu(j) = Vapp - abs(Erev_Cu(k)-Erev_Fe(k)) - I_an(j)*(r_sol+r_hardware) - abs(eta_an(j));
-        %eta_Cu(j) = eta_Cu(j) * (-1); %negative (cathodic overpotential)
-        eta_Cu(j) = -Vapp + Erev_Fe(k) + eta_an(j) + I_an(j)*(r_sol+r_hardware) - Erev_Cu(k);
-        I_Cu(j) = i_BV(eta_Cu(j), i0_Cu, alpha_Cu, z_Cu, temp)*S_cat;
-        I_cat(j) = I_Cu(j);
-        error(j) = 2*(I_an(j)-(-I_cat(j)))/((abs(I_cat(j))+abs(I_an(j)))); %I_cathode should be negative
-        pct_error(j) = abs(error(j))*100;
-        I_an(j+1) = I_an(j)-I_cat(j)/1000;
-        j = j + 1;
-        if j > max_iter
-            break;
-        end
+    %solve for currents and electrode potentials using fsolve
+    %wrapper function for cell solver
+    solver = @(x) cell_solver(x(1), x(2), x(3), V_app, r_sol(k), r_hardware, Erev_Fe(k), Erev_Cu(k), S_an, S_cat);
+    %initial guesses [I_an, E_an, E_cat]
+    x0 = [1, 0.2, -0.2];
+    x = fsolve(solver, x0);
+    I_an = x(1);
+    E_an(k) = x(2);
+    E_cat(k) = x(3);
+    I_Cu(k) = - I_an;
+    I_Fe(k) = I_an;
+    %4th order runge-kutta to solve mass balance differential equations
+	k1 = dC_dt(C_Cu2(k), C_Cu2_o, I_Cu(k), z_Cu);
+	k2 = dC_dt(C_Cu2(k)+h*k1/2, C_Cu2_o, I_Cu(k), z_Cu);
+	k3 = dC_dt(C_Cu2(k)+h*k2/2, C_Cu2_o, I_Cu(k), z_Cu);
+	k4 = dC_dt(C_Cu2(k)+h*k3, C_Cu2_o, I_Cu(k), z_Cu);
+    l1 = dC_dt(C_Fe2(k), C_Fe2_o, -I_Fe(k), z_Fe);
+    l2 = dC_dt(C_Fe2(k)+h*l1/2, C_Fe2_o, -I_Fe(k), z_Fe);
+    l3 = dC_dt(C_Fe2(k)+h*l2/2, C_Fe2_o, -I_Fe(k), z_Fe);
+    l4 = dC_dt(C_Fe2(k)+h*l3, C_Fe2_o, -I_Fe(k), z_Fe);
+    m1 = dC_dt(C_Fe3(k), C_Fe3_o, I_Fe(k), z_Fe);
+    m2 = dC_dt(C_Fe3(k)+h*m1/2, C_Fe3_o, I_Fe(k), z_Fe);
+    m3 = dC_dt(C_Fe3(k)+h*m2/2, C_Fe3_o, I_Fe(k), z_Fe);
+    m4 = dC_dt(C_Fe3(k)+h*m3, C_Fe3_o, I_Fe(k), z_Fe);
+    %Subplus function prevents negative concentrations in the cell
+	C_Cu2(k+1) = subplus(C_Cu2(k) + 1/6*h*(k1+2*k2+2*k3+k4));
+    C_Fe2(k+1) = subplus(C_Fe2(k) + 1/6*h*(l1+2*l2+2*l3+l4));
+    C_Fe3(k+1) = subplus(C_Fe3(k) + 1/6*h*(m1+2*m2+2*m3+m4));
+    if C_Cu2(k+1) == 0 | C_Fe2(k+1) == 0 | C_Fe3(k+1) == 0
+        break
     end
-	%Runge Kutta 4 to solve for ionic concentrations
-	k1 = dC_dt(C_Cu2(k), C_Cu2_o, I_Cu(j-1), z_Cu);
-	k2 = dC_dt(C_Cu2(k)+h*k1/2, C_Cu2_o, I_Cu(j-1), z_Cu);
-	k3 = dC_dt(C_Cu2(k)+h*k2/2, C_Cu2_o, I_Cu(j-1), z_Cu);
-	k4 = dC_dt(C_Cu2(k)+h*k3, C_Cu2_o, I_Cu(j-1), z_Cu);
-	C_Cu2(k+1) = C_Cu2(k) + 1/6*h*(k1+2*k2+2*k3+k4);
-	%Nernst
+	%Nernst potential calculation
 	Erev_Cu(k+1) = Eo_Cu - R*temp/(z_Cu*F)*log(1/(gamma_Cu2*C_Cu2(k+1)));
 	Erev_Fe(k+1) = Eo_Fe - R*temp/(z_Fe*F)*log(gamma_Fe2*C_Fe2(k+1)/gamma_Fe3*C_Fe3(k+1));
+end
+
+eta_an = E_an - Erev_Fe;
+eta_cat = E_cat - Erev_Cu;
+
+function Y = cell_solver(I_an, E_an, E_cat, V_app, r_sol, r_hardware, Erev_Fe, Erev_Cu, S_an, S_cat)
+    %units: I [A], E [V], V_app [V], r [ohms], S [cm^2]
+    global temp i0_Fe alpha_Fe z_Fe i0_Cu alpha_Cu z_Cu
+    eta_an = E_an - Erev_Fe;
+    eta_cat = E_cat - Erev_Cu;
+    Y(1) = E_an - E_cat + I_an*(r_sol+r_hardware) - V_app;
+    Y(2) = I_an - i_BV(eta_an, i0_Fe, alpha_Fe, z_Fe, temp)*S_an;
+    Y(3) = I_an + i_BV(eta_cat, i0_Cu, alpha_Cu, z_Cu, temp)*S_cat;
 end
