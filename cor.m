1e2ddcf73497923e0fc5565251af70ccf7412a0b
Aidan O'Gorman <aidan.h.ogorman@gmail.com>
2021-02-16 18:22:33 -0500

    Got rid of old files

--- cor.m
+++
@@ -1,6 +0,0 @@
-function [func] = cor(Ecorr, Erev, i0, alpha, z, temp)
-  %1 = Cu, 2= Fe  
-  i_Cu = i_BV(Ecorr-Erev(1), i0(1), alpha(1), z(1), temp);
-  i_Fe = i_BV(Ecorr-Erev(2), i0(2), alpha(2), z(2), temp);
-  func = i_Cu + i_Fe;
-  end
--- fun.m
+++
@@ -1,4 +0,0 @@
-  function [func] = fun(i_Cu(i), i_Fe(i))
-        func = i_Cu(i) + i_Fe(i);
-  end
-
